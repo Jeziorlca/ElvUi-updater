af47c8f6063e3f8aa01f101c293e51aed12492c8
Piotr Jeziorowski <pije@spreadshirt.private>
2019-03-21 10:03:16 +0100

    #2

--- Updater.py
+++ Updater.py
@@ -1,18 +1,29 @@
-from lxml import html
+#from lxml import html
 import requests
-def get_version ():
-    page = requests.get(' https://www.tukui.org/download.php?ui=elvui')
-    tree = html.fromstring(page.content)
-    f = open("demofile.txt", "w")
-    f.write(tree)
+#Base Settings
+url = "https://www.tukui.org/download.php?ui=elvui"
+game_location = "E:\\Nowy folder\\World of Warcraft\\"
+file_location = "_retail_\\Interface\\AddOns\\ElvUI\\ElvUI.toc"
+#def get_version ():
+#    page = requests.get(url)
+#    tree = html.fromstring(page.content)
+#    f = open("demofile.txt", "w")
+#    f.write(tree)
     #get version form pharsing website code https://www.tukui.org/download.php?ui=elvui
-    return True
+#    return True
 # Function that checks current version available
-def installed_version (path):
-    #get current version installed in file ("_retail_\Interface\AddOns\ElvUI\ElvUI.toc", r)
-    return False
-if installed_version == get_version:
-    print ("You are up to date")
-else:
+
+###### DONE:######
+#def installed_version ():
+    #Opens file and checks the current version installed of ElvUi
+#    f = open (game_location + file_location, "r")
+#    tmp = f.readlines()[2]
+#    return tmp[12:17:1]
+#print (installed_version())
+##################
+
+#if installed_version() == get_version():
+#    print ("You are up to date")
+#else:
 #download latest package and extract to "_retail_\Interface\AddOns"
-    print ("version will be updated")
+#    print ("version will be updated")
