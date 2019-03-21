cce5e804fdc6c5af2db505e39fbd3510ff75c0ff
Piotr Jeziorowski <jezior_lca@wp.pl>
2019-03-21 14:26:18 +0100

    Added function that checks what version is available to download from url

--- Updater.py
+++ Updater.py
@@ -1,18 +1,17 @@
-#from lxml import html
 import requests
 import re
 #Base Settings
 url = "https://www.tukui.org/download.php?ui=elvui"
+page = requests.get(url)
 game_location = "E:\\Nowy folder\\World of Warcraft\\"
 file_location = "_retail_\\Interface\\AddOns\\ElvUI\\ElvUI.toc"
-#def get_version ():
-#    page = requests.get(url)
-#    tree = html.fromstring(page.content)
-#    f = open("demofile.txt", "w")
-#    f.write(tree)
-    #get version form pharsing website code https://www.tukui.org/download.php?ui=elvui
-#    return True
-# Function that checks current version available
+
+def get_version():
+    pattern = "/downloads/elvui-"r"\d\d\.\d\d"
+    for i, line in enumerate(page.text.split("\n")):
+        for match in re.finditer(pattern, line):          
+            return match.group().split("-")[-1].strip()
+print (get_version())
 
 def installed_version ():
     pattern = re.compile("## Version: "r"\d\d\.\d\d")
@@ -21,8 +20,8 @@ def installed_version ():
             return match.group().split(":")[-1].strip()
 print(installed_version())
 
-#if installed_version() == get_version():
-#    print ("You are up to date")
-#else:
+if installed_version() == get_version():
+    print ("You are up to date")
+else:
 #download latest package and extract to "_retail_\Interface\AddOns"
-#    print ("version will be updated")
+    print ("version will be updated")
