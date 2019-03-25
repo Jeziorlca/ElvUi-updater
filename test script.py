20e5d46f96c83f148eb45f4ef383891a29f5fdf4
Piotr Jeziorowski <jezior_lca@wp.pl>
2019-03-25 10:01:32 +0100

    Delete unecesary files

--- test script.py
+++
@@ -1,7 +0,0 @@
-from lxml import html
-import requests
-page = requests.get('https://www.tukui.org/download.php?ui=elvui')
-#tree = html.fromstring(page.content)
-#tmp = page.text
-f = open("tmp.txt", "w")
-f.write(page.text)
